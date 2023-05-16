207ccfc65aadc374458d4a25a6d85256b670ed40
Guilherme Steglich de Oliveira <guicrga@gmail.com>
2023-05-16 12:10:54 -0300

    :sparkles: feat: Challenge four of #7DaysOfCode

---
+++ d4.py
@@ -0,0 +1,41 @@
+"""
+Nesse desafio decidi por utilizar as ferramentas nativas do python com listas para implementar a fila
+Ou seja, append e pop para enfileirar e desenfileirar respectivamente.
+"""
+
+
+class Pedido:
+    def __init__(self, numero, nomeCliente, items, valor):
+        self.numero = numero
+        self.nomeCliente = nomeCliente
+        self.items = items
+        self.valor = valor
+
+
+class FilaDePedidos:
+    def __init__(self):
+        self.valores = []
+
+    def enfileirar(self, pedido):
+        self.valores.append(pedido)
+
+    def desenfileirar(self):
+        pedido_atendido = self.valores.pop(0)
+        return print(
+            f"O pedido {pedido_atendido.numero} do cliente {pedido_atendido.nomeCliente} com os items {pedido_atendido.items} começou a ser preparado."
+        )
+
+
+pedido1 = Pedido(1, "Guilherme", ["hamburguer"], 10.99)
+pedido2 = Pedido(2, "Geromel", ["hamburguer", "pizza"], 25.99)
+pedido3 = Pedido(3, "Guilherme", ["coca zero"], 4.99)
+
+fila = FilaDePedidos()
+
+fila.enfileirar(pedido1)
+fila.enfileirar(pedido2)
+fila.enfileirar(pedido3)
+
+fila.desenfileirar()
+fila.desenfileirar()
+fila.desenfileirar()
